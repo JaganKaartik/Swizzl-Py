406ce53afd2cf03bd3e75f367358e1e09b46c5fc
athulp7 <44413937+athulp7@users.noreply.github.com>
2019-04-28 13:53:37 +0530

    Updates Routes.py

--- xplore/routes.py
+++ xplore/routes.py
@@ -1,22 +1,58 @@
+#Import Statements
+
 from flask import render_template, flash, redirect, url_for, request
 from xplore import app, db, bcrypt
-from xplore.forms import RegistrationForm, LoginForm, UpdateAccountForm, PickGenre
+from xplore.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from xplore.models import User
 from flask_login import login_user, current_user, logout_user, login_required
 from xplore.crawler import scraping
 
+
+#HomePage or the Root File
+
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('pref.html')
+
+#Login Required for the User Choosing Preferences
+
 
 @login_required
-@app.route('/dashboard')
-def web():
-    Gdict = scraping.scrape()
-    return render_template('web.html',dict = Gdict,title = 'web')
+@app.route('/pref')
+def prefform():
+   return render_template('pref.html')
 
-#User_Registration
+#View All Feeds
+
+@login_required
+@app.route('/feedboard',methods = ['POST', 'GET'])
+def feedboard():
+    if request.method == 'POST':
+        genre = request.form['Genre']
+        website = request.form['Website']
+    else:
+        return redirect(url_for('home'))
+    if not request.form['Genre']:
+        flash('Please enter Genre')
+        return redirect(url_for('home'))
+    if(website == 'NY'):
+        Gdict = scraping.nyScrape(genre)
+    elif(website == 'GL'):
+        Gdict = scraping.glScrape(genre)
+    elif(website == 'GA'):
+        Gdict = scraping.guaScrape(genre)
+    else:
+        flash('Please enter correct abbrevation')
+        return redirect(url_for('home'))
+    print(Gdict)
+    if Gdict == "Empty":
+        flash('No news in the genre, please re-enter genre','danger')
+        return redirect (url_for('home'))
+
+    return render_template('feeds.html',dict = Gdict,title = 'feed')
+
+#User_Registration and Redirect to Log in page
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
@@ -30,10 +66,10 @@ def register():
         db.session.commit()
         flash(f'Account created for {form.username.data}!', 'success!')
         return redirect(url_for('login'))
-    return render_template('register.html', title='Register', form=form)
+    return render_template('register.html', title='Register', form = form)
 
 
-#User_Login and Redirect to Login
+#User_Login and Redirect to Home Page
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
@@ -44,27 +80,34 @@ def login():
         user = User.query.filter_by(username=form.username.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
-            return redirect(url_for('front'))
+            return redirect(url_for('home'))
         else:
             flash('Login Unsuccessful. Please check username and password', 'danger')
-    return render_template('login.html', title='Login', form=form)
+    return render_template('login.html', title='Login', form = form)
+
 
 @app.route("/logout")
+@login_required
 def logout():
     logout_user()
+    flash('You have successfully logged out', 'success')
     return redirect(url_for('home'))
 
-"""
-@app.route("/account")
-@login_required
-def account():
-    return render_template('account.html', title='Account')
-"""
 
+@app.route("/feeds")
+@login_required
+def feeds():
+    return render_template('feeds.html', title = 'feed')
+
+"""
 @app.route("/front")
 def front():
-    return render_template('front.html', title='Front')
+    return render_template('front.html', title='front')
+"""
 
+""" User Profile Management """
+
+#Updating account Information
 
 @app.route("/account", methods=['GET', 'POST'])
 @login_required
@@ -77,4 +120,4 @@ def account():
         return redirect(url_for('account'))
     elif request.method == 'GET':
         form.username.data = current_user.username
-    return render_template('account.html', title='Account', form=form)
+    return render_template('account.html', title='Account', form = form)
