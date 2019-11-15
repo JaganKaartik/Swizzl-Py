cbd9ebe2ab0931757344437ce8785d8791600a1b
JaganKaartik <kaartikjagan@gmail.com>
2019-11-15 22:56:29 +0530

    Yahoo Crawler Working Tested

--- swizzl/crawler/newsfetch.py
+++ swizzl/crawler/newsfetch.py
@@ -24,64 +24,55 @@ def linkText(link):
 	return article_text
 
 def YahooFetch():
-
-	# Using Yahoo News RSS Feeds
-
-	url = "https://news.yahoo.com/rss/"
-	url = requests.get(url)
+    url = "https://news.yahoo.com/rss/"
+    url = requests.get(url)
     soup = BeautifulSoup(url.text,'xml')
-
-    """
-    Required items to fetch
-
-    <item>
-    	<title>
-    	<description>
-		<link>
-		<pubDate>
-	</item>
-
-    """
-
+    
     try:
         # Fetch All Required items
 
         titles = soup.findAll('title')
         links = soup.findAll('link')
-        descriptions = soup.findAll('description')
         pubDates = soup.findAll('pubDate')
 
     except Exception as e :
         # Return Empty if titles, links, descriptions, pubdates not found
+       
         print(e)
         return "Error"
 
-   	# We don't want the first elements as these are just metadata
+    # We don't want the first elements as these are just metadata
+    
+    titles.pop(0)
     titles.pop(0)
     links.pop(0)
+    links.pop(0)
     pubDates.pop(0)
-    descriptions.pop(0)
 
-    # Dictionary to hold crawled information
+    #Dictionary to hold crawled information
 
     FeedDict = {}
     temp = []
+    temptext = []
     try:
         for i in titles:
             temp.append(i.get_text())
             FeedDict['title'] = temp
-        temp.clear()
+        temp = []
         for i in links:
             temp.append(i.get_text())
+            temptext.append(linkText(i.get_text()))
             FeedDict['link'] = temp
-        temp.clear()
+            FeedDict['linktext'] = temptext
+        temp = []
         for i in pubDates:
-            te.append(i.get_text())
+            temp.append(i.get_text())
             FeedDict['pubdate'] = temp
-        temp.clear()
-       
+        
+        print("Success")
     except Exception as e :
         print(e)
         return "Error"
-
+    
     return FeedDict
+
