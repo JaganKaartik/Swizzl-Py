59a13c7cdbd8171d0ed00a0e15f1d21b768c785d
JaganKaartik <kaartikjagan@gmail.com>
2019-11-16 11:44:10 +0530

    Minor Fix

---
+++ swizzl/services/mlearning/lda.py
@@ -0,0 +1,14 @@
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+import string
+
+stop = set(stopwords.words('english'))
+exclude = set(string.punctuation)
+lemma = WordNetLemmatizer()
+def clean(doc):
+    stop_free = " ".join([i for i in doc.lower().split() if i not in stop])
+    punc_free = ''.join(ch for ch in stop_free if ch not in exclude)
+    normalized = " ".join(lemma.lemmatize(word) for word in punc_free.split())
+    return normalized
+
+doc_clean = [clean(doc).split() for doc in doc_complete]   
