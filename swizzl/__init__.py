856477b390cce361f0d9a3379b4c81b072aa5afb
JaganKaartik <kaartikjagan@gmail.com>
2019-11-27 23:55:43 +0530

    Minor Fix

--- swizzl/__init__.py
+++ swizzl/__init__.py
@@ -6,9 +6,7 @@ from flask_admin import Admin
 from swizzl.services import newsfetch as snf
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from swizzl.models import User, ViewPosts
-from swizzl import routes
-from swizzl.routes import MyModelView
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
@@ -16,7 +14,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///testdatabase.db'
 
 
 db = SQLAlchemy(app)
-# migrate = Migrate(app, db)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
@@ -25,6 +22,10 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 
 manager.add_command('db',MigrateCommand)
+
+from swizzl.models import User, ViewPosts
+from swizzl import routes
+from swizzl.routes import MyModelView
 
 admin.add_view(MyModelView(User, db.session))
 admin.add_view(MyModelView(ViewPosts, db.session))
