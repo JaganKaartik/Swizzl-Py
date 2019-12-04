b8ada212052f7680d2f48327d0f1d36436ee701b
JaganKaartik <kaartikjagan@gmail.com>
2019-12-04 09:56:15 +0530

    Dark UI

--- swizzl/static/script.js
+++ swizzl/static/script.js
@@ -1,5 +1,31 @@
 function myFunction() 
 {
     var element = document.getElementById("nav");
-    alert(element.className);
+    var card = document.getElementsByClassName("card");
+    var cardchildren = document.getElementsByClassName("card").children;
+    var i,j;
+    if(element.classList.contains("light-blue"))
+    {
+        element.classList.remove("light-blue");
+        element.classList.add("blue-grey");
+        for(i=0;i<card.length;++i)
+        {
+            card[i].classList.add("blue-grey");
+            for (i = 0; i < c.length; i++) 
+            {
+                txt = txt + c[i].tagName + "<br>";
+            }
+            card[i].classList.add("white-text");
+        }
+    }
+    else if(element.classList.contains("blue-grey"))
+    {
+        element.classList.remove("blue-grey");
+        element.classList.add("light-blue");
+        for(i=0;i<card.length;++i)
+        {
+            card[i].classList.remove("blue-grey");
+            card[i].classList.remove("white-text");
+        }
+    }
 }
