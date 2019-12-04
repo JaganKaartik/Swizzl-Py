9ab63e39cb8dbd491eebf0978d5c0b3f600ce890
JaganKaartik <kaartikjagan@gmail.com>
2019-12-04 10:28:49 +0530

    Added Navbar Dark Mode

--- swizzl/static/script.js
+++ swizzl/static/script.js
@@ -1,31 +1,14 @@
 function myFunction() 
 {
     var element = document.getElementById("nav");
-    var card = document.getElementsByClassName("card");
-    var cardchildren = document.getElementsByClassName("card").children;
-    var i,j;
     if(element.classList.contains("light-blue"))
     {
         element.classList.remove("light-blue");
         element.classList.add("blue-grey");
-        for(i=0;i<card.length;++i)
-        {
-            card[i].classList.add("blue-grey");
-            for (i = 0; i < c.length; i++) 
-            {
-                txt = txt + c[i].tagName
-            }
-            card[i].classList.add("white-text");
-        }
     }
     else if(element.classList.contains("blue-grey"))
     {
         element.classList.remove("blue-grey");
         element.classList.add("light-blue");
-        for(i=0;i<card.length;++i)
-        {
-            card[i].classList.remove("blue-grey");
-            card[i].classList.remove("white-text");
-        }
     }
 }
